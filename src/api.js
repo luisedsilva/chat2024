d393bdac2f376ea760c53d29f31fb44d0960ad06
luisedsilva <135769052+luisedsilva@users.noreply.github.com>
2024-03-16 15:46:28 -0300

    Atualizado

--- src/api.js
+++ src/api.js
@@ -24,3 +24,33 @@ app.use("/salas",router.get("/salas", (req, res, next) => {
 }));
 
 module.exports=app;
+
+app.use("/salas",router.get("/salas", async (req, res,next) => {
+    if(await token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)) {
+    let resp= await salaController.get();
+    res.status(200).send(resp);
+  }else{
+    res.status(400).send({msg:"Usuário não autorizado"});
+  }
+}))
+
+app.use("/sala/entrar", router.put("/sala/entrar", async (req, res)=>{
+    if(!token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
+    let resp= await salaController.entrar(req.headers.iduser, req.query.idsala);
+    res.status(200).send(resp);
+  }))
+
+  app.use("/sala/mensagem/", router.post("/sala/mensagem", async (req, res) => {
+    if(!token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
+    let resp= await salaController.enviarMensagem(req.headers.nick, req.body.msg,req.body.idSala);
+    res.status(200).send(resp);
+  }))
+  
+  
+
+app.use("/sala/mensagens/", router.get("/sala/mensagens", async (req, res) => {
+    if(!token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
+    let resp= await salaController.buscarMensagens(req.query.idSala, req.query.timestamp);
+    res.status(200).send(resp);
+  }))
+  
--- src/models/db.js
+++ src/models/db.js
@@ -18,3 +18,24 @@ async function findALL(collection) {
 }
 
 module.exports = { findALL}
+
+let findAll = async (collection)=>{
+    const db = await connect();
+    return await db.collection(collection).find().toArray();
+  }
+
+  let findOne = async (collection, _id)=>{
+    const db = await connect();
+    let obj= await db.collection(collection).find({'_id':new ObjectId(_id)}).toArray();
+    if(obj)
+      return obj[0];
+    return false;
+  }
+  
+  
+  let updateOne= async (collection, object, param)=>{
+    const db = await connect();
+    let result= await db.collection(collection).updateOne(param, { $set: object} );
+    return result;
+  }
+  
